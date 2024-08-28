6659bd5ac19e96906c20d4da2ce39ace4bc146ce
Mefiseru <62016614+Mefiseru@users.noreply.github.com>
2024-08-28 16:59:41 +0200

    Making code more independent from user

--- src/system_info.py
+++ src/system_info.py
@@ -1,7 +1,9 @@
 # DBDriveSync/src/system_info.py
 
+import os
 import platform
 import subprocess
+import configparser
 
 def identify_system():
     """
@@ -19,16 +21,32 @@ def list_postgres_databases():
     Returns:
         list: A list of database names.
     """
+    config = configparser.ConfigParser()
+    config.read('config/config.ini')
+
+    # Read PostgreSQL credentials from config file
+    host = config['POSTGRESQL']['HOST']
+    port = config['POSTGRESQL']['PORT']
+    user = config['POSTGRESQL']['USER']
+    password = config['POSTGRESQL']['PASSWORD']
+
+    # Set the password environment variable
+    os.environ['PGPASSWORD'] = password
+
     system = identify_system()
     databases = []
-    
+
     try:
         # Running psql command to list databases
-        output = subprocess.check_output(['psql', '-l'], universal_newlines=True)
+        output = subprocess.check_output(['psql', '-h', host, '-p', port, '-U', user, '-l'], universal_newlines=True)
         for line in output.splitlines():
             if '|' in line and not line.startswith(' '):
                 db = line.split('|')[0].strip()
                 databases.append(db)
+    except FileNotFoundError as e:
+        print("Error: 'psql' command not found. Please ensure PostgreSQL is installed and the 'psql' command is available in your PATH.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error listing databases: {e.output}")
     except Exception as e:
         print(f"Error listing databases: {e}")
     
